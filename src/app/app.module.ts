55c278bc42f46659a7b30728d7989c914aa1c28c
kyara-code <chiara.ko.26@gmail.com>
2022-07-08 11:24:37 +0200

    homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FunfactsComponent } from './funfacts/funfacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    FunfactsComponent
   ],
   imports: [
     BrowserModule
